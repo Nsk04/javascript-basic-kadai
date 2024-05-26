ee8dd9312abca8e3058862656e701af10e7b2c05
Nsk04 <s.nomura@gmail.com>
2024-05-27 00:30:55 +0900

    kadai_020の内容を追加

---
+++ kadai_020/event.js
@@ -0,0 +1,15 @@
+// btnというidを持つHTML要素を取得し、定数に代入する
+const btn = document.getElementById('btn');
+// textというidを持つHTML要素を取得し、定数に代入する
+const text = document.getElementById('text');
+
+// HTML要素がクリックされたときにイベント処理を実行する
+btn.addEventListener('click', () => {
+
+  // 作成したli要素に「ボタンをクリックしました」というテキストを追加する
+  text.textContent = 'ボタンをクリックしました';
+
+  // 作成したli要素をul要素の子要素として末尾に追加する
+  parentList.appendChild(text);
+
+});
